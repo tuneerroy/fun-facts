b0078e4ab3472d45fa71f38d4f298915cd93a544
Tuneer Roy <53184517+tuneerroy@users.noreply.github.com>
2024-11-13 20:03:31 -0500

    feat: more complicated qc

--- data/quality_control.py
+++ data/quality_control.py
@@ -5,18 +5,57 @@ def judge_data(fact_or_fiction):
         return "rejected", None
 
 
+moderations = []  # imagine this is a list of moderators
+
+
 def quality_contorl(data):
     res = []
     for is_fact, value in data:
-        approval, score = judge_data(is_fact)
+        approval_and_scores = []
+        for moderator in moderations:
+            approval, score = moderator(is_fact)
+            approval_and_scores.append((approval, score))
+
         res.append(
-            {"is_fact": is_fact, "value": value, "approval": approval, "score": score}
+            {
+                "is_fact": is_fact,
+                "value": value,
+                "approvals_and_scores": approval_and_scores,
+            }
         )
     return res
 
 
+def significant_majority_vote(data, threshold=0.5):
+    approved = 0
+    total_score = 0
+    for item in data:
+        approval, score = item
+        if approval == "approved":
+            approved += 1
+            total_score += score
+    return approved / len(data) > threshold, (
+        (total_score / approved) if approved > 0 else 0
+    )
+
+
+def filter_for_approved_data(data):
+    res = []
+    for item in data:
+        is_approved, score = significant_majority_vote(item["approvals_and_scores"])
+        if is_approved:
+            res.append(
+                {
+                    "is_fact": item["is_fact"],
+                    "value": item["value"],
+                    "score": score,
+                }
+            )
+    return res
+
+
 def aggregation(data):
-    approved_data = [item for item in data if item["approval"] == "approved"]
+    approved_data = filter_for_approved_data(data)
     facts = [
         {"fact": item["value"], "score": item["score"]}
         for item in approved_data
